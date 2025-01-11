b2bc68f0fc99fbd469f227a03a26802c74e81faa
R-JANG-98 <msgpigeon@gmail.com>
2025-01-11 17:39:30 +0900

    .
    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:dingdong_flutter_teacher/firebase_options.dart';
 import 'package:dingdong_flutter_teacher/screen/home_screen.dart';
 import 'package:dingdong_flutter_teacher/screen/login_page.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -10,11 +12,31 @@ void main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+
+  Dio dio = Dio();
+  runApp(MyApp(dio: dio));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Dio dio;
+  const MyApp({super.key, required this.dio});
+
+  Future<int> fetchTeacherId(User? user) async {
+    if (user == null) return 0;
+    final serverURL = dotenv.env['FETCH_SERVER_URL'];
+    try {
+      final response = await dio.get('$serverURL/user/${user.email}');
+      if (response.statusCode == 200) {
+        final data = response.data;
+        return data is int ? data : int.tryParse(data.toString()) ?? 0;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error fetching teacherId: $e");
+      }
+    }
+    return 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +45,28 @@ class MyApp extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasData) {
-            return HomeScreen(user: snapshot.data!);
+            return FutureBuilder<int>(
+              future: fetchTeacherId(snapshot.data),
+              builder: (context, teacherIdSnapshot) {
+                if (teacherIdSnapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (teacherIdSnapshot.hasData) {
+                  return HomeScreen(
+                    user: snapshot.data!,
+                    teacherId: teacherIdSnapshot.data!,
+                  );
+                } else {
+                  return LoginPage();
+                }
+              },
+            );
           } else {
-            return const LoginPage();
+            return LoginPage();
           }
         },
       ),
     );
   }
 }
-
