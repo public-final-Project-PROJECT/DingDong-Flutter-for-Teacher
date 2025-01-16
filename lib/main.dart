5f11bb178bd1b42532a90ce3fee888fcb9328417
R-JANG-98 <msgpigeon@gmail.com>
2025-01-16 16:13:03 +0900

    노티 어쩌고 홈 화면 알림 기록 확인 제거

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,14 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
